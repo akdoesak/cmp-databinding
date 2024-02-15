d817f686d5bdef8cded809a57c3b6a5d5692f5f7
akdoesak <akdoesannakaminska@gmail.com>
2024-02-15 11:35:51 +0100

    ChildComponent

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,11 +1,16 @@
-import { Component, Input } from '@angular/core';
+import {AfterContentInit, Component, ContentChild, ElementRef, Input} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrl: './server-element.component.css'
 })
-export class ServerElementComponent {
+export class ServerElementComponent  implements AfterContentInit {
   @Input('srvElement') element:{name:string, type:string, content:string};
   @Input() name : string;
+  @ContentChild('contentParagraph', { static: true }) paragraph :ElementRef;
+
+  ngAfterContentInit(): void {
+    console.log('After content init ' + this.paragraph.nativeElement.content)
+  }
 }
